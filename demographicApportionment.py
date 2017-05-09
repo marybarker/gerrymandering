515e5444785d9e73fc07a92b573cea6e0262c8d4
marybarker <marybarker103@gmail.com>
2017-05-08 20:05:33 -0500

    Finished determining the portion of blockgroup in each VTD for multiplificyfacations.

--- demographicApportionment.py
+++ demographicApportionment.py
@@ -18,6 +18,8 @@ vtdFile     = './precinct/precinct.shp'
 #groupFile   = '/home/joseph/Dropbox/gerrymandering/PA_BlockGroupData.csv'
 groupFile   = './PA_BlockGroupData.csv'
 vtdInfoFile = './vtdstats.csv'
+assignmentfile = './blockframe.csv'
+#blockFrame = pd.read_csv(assignmentfile)
 
 ds = ogr.Open(blockFile)
 lyr = ds.GetLayer(0)
@@ -60,7 +62,7 @@ for i in range(len(blocks)):
 
 blockFrame = pd.DataFrame([(block.BLOCKID10, block.BLOCKID10[:-3], block.POP10) for block in blocks], columns = ('BLOCKID', 'GROUPID', 'population'))
 blockFrame['VTDID'] = [x[1] for x in blockVTD]
-blockFrame.to_csv('./blockframe.csv', index=False)
+blockFrame.to_csv(assignmentFile, index=False)
 
 problems = blockFrame.loc[blockFrame.VTDID == "NO VTD COVERS CENTROID"].copy()
 closestVTD = [] #This currently looks at the nearest centroid.
@@ -80,6 +82,39 @@ ds = ogr.Open(precinctBoundaryFile)
 lyr = ds.GetLayer(0)
 precincts = features(lyr)
 
+#VTD block group pairings.
+# VTD | block group | % of block group
 
+vtdgrouppairing = { "VTD"      : [],
+                    "GROUP"    : [],
+                    "PCTGROUP" : [] }
 
+i = 0
+numgroups = groupStats.shape[0]
+grouplist = list(set(blockFrame.GROUPID))
 
+for group in grouplist:
+    temp = blockFrame.ix[blockFrame.GROUPID == group, ["population", "VTDID"]]
+    grouppop = sum(temp.population)
+
+    if grouppop != 0:
+        for vtd in temp.VTDID.unique():
+            vtdgrouppairing['VTD'].append(vtd)
+            vtdgrouppairing['GROUP'].append(group)
+            vtdgrouppairing['PCTGROUP'].append(float(sum(temp.population[temp.VTDID == vtd]))/grouppop)
+    else:
+        for vtd in temp.VTDID.unique():
+            vtdgrouppairing['VTD'].append(vtd)
+            vtdgrouppairing['GROUP'].append(group)
+            vtdgrouppairing['PCTGROUP'].append(float(1)/len(temp.VTDID.unique()))
+    i = i + 1
+    print("Group %d of %d"%(i, numgroups))
+
+#vpgtlist = list(set([x for x in zip(vtdgrouppairing['VTD'], vtdgrouppairing['GROUP'], vtdgrouppairing['PCTGROUP'])]))
+#vgpFrame = pd.DataFrame(vpgtlist, columns=["VTD", "GROUP", "PCTGROUP"])
+vgpFrame = pd.DataFrame(vtdgrouppairing)
+
+problems = [vgpFrame.GROUP[i] for i in vgpFrame.index if \
+            abs(sum(vgpFrame.PCTGROUP[vgpFrame.GROUP == vgpFrame.GROUP[i]]) - 1) > 0.001]
+
+vgpFrame.ix[vgpFrame.GROUP == problems[0]]
