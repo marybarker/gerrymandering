ee70ef4a23c139f5cdd3c1ebf10d94007f4777d5
drscook <scook@tarleton.edu>
2017-06-03 14:11:40 -0500

    major revisions

--- Cook/main_current.py
+++ Cook/main_current.py
@@ -4,7 +4,12 @@ from helper.pycuda_setup import *
 
 
 kernel_code = """
-    #include <stdio.h>    
+    #include <stdio.h>
+    #include <curand.h>
+    #include <curand_kernel.h>
+
+extern "C"
+{
     __device__ void to_shared(ushort *A, ushort *A_shr, ushort size, ushort threadIdLoc) {
         const ushort threads_per_block = blockDim.z * blockDim.y * blockDim.x;
         ushort idx;
@@ -23,115 +28,128 @@ kernel_code = """
 
 
     __device__ void print_shared(ushort *A_shr, ushort size) {
-        uint idx;        
+        ushort idx;        
         for(idx = 0; idx < size; idx++) {
-            printf("A_shr[%d] = %d\\n",idx,A_shr[idx]);
+            printf("A_shr[%u] = %u\\n", idx, A_shr[idx]);
         }
     }
 
-    __device__ void get_components_local(ushort *nbrs, ushort *nbr_rng, ushort *color, ushort *component, ushort *num_comps, ushort w) { 
-        printf("num vtds = %d, w = %d, num_comps = %d\\n",NUM_VTDS, w, (*num_comps));
-        ushort u, v, idx, orig_comp, old_comp, new_comp;
+    __device__ void print_vtd(ushort *clr, ushort *cmp, ushort v) {
+        printf("vtd%u:clr%u:cmp%u  ", v, clr[v], cmp[v]);
+    }
+
+
+    __device__ void print_all(ushort *clr, ushort *cmp) {
+        ushort v;
+        for(v = 0; v < NUM_VTDS; v++) {
+            print_vtd(clr, cmp, v);
+        }
+        printf("\\n");
+    }
+
+
+    __device__ void get_cmps_local(ushort *nbrs, ushort *nbr_rng, ushort *clr, ushort *cmp, ushort *num_cmps, ushort w) {         
+        printf("Trying to assign vtd%u to a connected cmp.\\n", w);
+        print_all(clr, cmp);
+        ushort u, v, idx, orig_cmp, old_cmp, new_cmp;
         
-        orig_comp = component[w];
-        new_comp = (*num_comps);
-        component[w] = new_comp;
-        printf("num vtds = %d, w = %d, num_comps = %d\\n",NUM_VTDS, w, (*num_comps));
-        printf("\\n\\nTrying to assign vtd %d to a connected component.  Previously in component %d\\n", w, orig_comp);
-        printf("There are currently %d connected components\\n",(*num_comps));
+        orig_cmp = cmp[w];
+        new_cmp = (*num_cmps);
+        cmp[w] = new_cmp;                
+        printf("vtd%u:clr%u:cmp%u -> cmp%u\\n", w, clr[w], orig_cmp, new_cmp);
         for(idx = nbr_rng[w]; idx < nbr_rng[w+1]; idx++) {
             v = nbrs[idx];
-            printf("\\nvtd %d, color %d, component %d\\n", w, color[w], component[w]);
-            printf("Checking neighbor vtd %d, color %d, component %d\\n", v, color[v], component[v]);
-            if(color[v] == color[w]) {
-                printf("Colors match\\n");
-                if(component[v] < new_comp) {
-                    new_comp = component[v];
-                    printf("Smaller component found!!\\nvtd %d: component %d -> %d\\n", w, component[w], new_comp);
-                    component[w] = new_comp;
+            printf("Checking neighbor vtd%u:clr%u:cmp %u\\n", v, clr[v], cmp[v]);
+            if(clr[v] == clr[w]) {
+                printf("Clrs match\\n");
+                if(cmp[v] < new_cmp) {
+                    new_cmp = cmp[v];
+                    printf("Smaller cmp found!!\\n");
+                    printf("vtd%u:clr%u:cmp%u -> cmp%u\\n", w, clr[w], cmp[w], new_cmp);
+                    cmp[w] = new_cmp;
                 }
                 else {
-                    printf("Larger component found.  Not reassigning.");
+                    printf("Larger cmp found.  Not reassigning.");
                 }
             }
-            if(new_comp == 0){
+            if(new_cmp == 0){
                 break;
             }
         }
-        component[w] = new_comp;
-        if(new_comp == (*num_comps)) {
-            printf("Incrementing num_comps\\n");
-            (*num_comps)++;
+        cmp[w] = new_cmp;
+        if(new_cmp == (*num_cmps)) {
+            printf("Incrementing num_cmps\\n");
+            (*num_cmps)++;
         }        
 
 
         char found = 0;
-        if(new_comp != orig_comp) {
-            printf("\\nvtd %d switched from component %d to component %d.  Let's check if somebody else is still in component %d\\n",w, orig_comp, new_comp, orig_comp);
+        if(new_cmp != orig_cmp) {
+            printf("\\nvtd%u switched from cmp%u to cmp%u.  Let's check if somebody else is still in cmp%u\\n",w, orig_cmp, new_cmp, orig_cmp);
             for(v = 0; v < NUM_VTDS; v++) {
-                if(component[v] == orig_comp) {
-                    printf("Yes, vtd %d is still in component %d\\n", v, component[v]);
+                if(cmp[v] == orig_cmp) {
+                    printf("Yes, vtd%u:clr%u:cmp%u\\n", v, clr[v], cmp[v]);
                     found = 1;
                     break;
                 }
             }
             if(found == 0) {
-                (*num_comps)--;                
-                printf("No, component %d is now empty.  I will relabel every vtd in component %d as component %d\\n", orig_comp, (*num_comps), orig_comp);
-                printf("There are currently %d connected components\\n", (*num_comps));
+                (*num_cmps)--;                
+                printf("No, cmp%u is now empty.  I will relabel every vtd in cmp%u -> cmp%u\\n", orig_cmp, (*num_cmps), orig_cmp);
                 for(v = 0; v < NUM_VTDS; v++) {
-                    if(component[v] == (*num_comps)){
-                        printf("vtd %d: component %d -> %d\\n", v, component[v], old_comp);
-                        component[v] = orig_comp;
+                    if(cmp[v] == (*num_cmps)){
+                        printf("vtd%u:clr%u:cmp%u -> cmp%u\\n", v, clr[v], cmp[v], orig_cmp);
+                        cmp[v] = orig_cmp;
                     }
                 }
             }
         }
 
-        printf("\\nNow we check if this fused >=2 previously distinct components into 1 big component\\n");
+        printf("\\nNow we check if this fused >=2 previously distinct cmps into 1 big cmp\\n");
         for(idx = nbr_rng[w]; idx < nbr_rng[w+1]; idx++) {
             v = nbrs[idx];
-            printf("Checking neighbor vtd %d, color %d, component %d\\n", v, color[v], component[v]);
-            if(color[v] == color[w]) {
-                if(component[v] > component[w]) {
-                    (*num_comps)--;
-                    old_comp = component[v];
-                    new_comp = component[w];
+            printf("Checking neighbor vtd%u:clr %u:cmp%u\\n", v, clr[v], cmp[v]);
+            if(clr[v] == clr[w]) {
+                if(cmp[v] > cmp[w]) {
+                    (*num_cmps)--;
+                    old_cmp = cmp[v];
+                    new_cmp = cmp[w];
                     
-                    printf("Yes, component %d fused with component %d.\\nRelabeling all vtds in component %d as %d and all vtds in component %d as %d.\\n", old_comp, new_comp, old_comp, new_comp, (*num_comps), old_comp);
+                    printf("Yes, cmp%u fused with cmp%u.\\nRelabeling all vtds in cmp%u -> cmp%u and all vtds in cmp%u -> cmp%u.\\n", old_cmp, new_cmp, old_cmp, new_cmp, (*num_cmps), old_cmp);
                     
                     for(u = 0; u < NUM_VTDS; u++) {
-                        if(component[u] == old_comp) {
-                            printf("vtd %d: component %d -> %d\\n", u, component[u], new_comp);
-                            component[u] = new_comp;                            
-                        } else if(component[u] == (*num_comps)) {
-                            printf("vtd %d: component %d -> %d", u, component[u], old_comp);
-                            component[u] = old_comp;                            
+                        if(cmp[u] == old_cmp) {
+                            printf("vtd %u: cmp %u -> %u\\n", u, cmp[u], new_cmp);
+                            cmp[u] = new_cmp;                            
+                        } else if(cmp[u] == (*num_cmps)) {
+                            printf("vtd %u: cmp %u -> %u", u, cmp[u], old_cmp);
+                            cmp[u] = old_cmp;                            
                         }
                     }
                 }
             }
         }
-        printf("\\nThere are currently %d connected components\\n", (*num_comps));
-        for(v = 0; v < NUM_VTDS; v++) {
-            printf("%d:%d  ", v, component[v]);
-        }
         
-        printf("w = %d",w);
-        printf("\\nThere are currently %d connected components\\n", (*num_comps));
-        printf("END OF ASSIGNMENT PROCESS FOR vtd %d\\n\\n\\n", w);        
+        print_all(clr, cmp);
+        printf("END OF ASSIGNMENT PROCESS FOR vtd %u\\n", w);
     }
 
 
+    __device__ void get_TV_local(ushort *pop_clr, float pop_tot, float unif_dens, float *TV_vec, ushort c) {
+        TV_vec[c] = fabs(pop_clr[c] / pop_tot - unif_dens);
+    }
 
-    __global__ void gerrymander_kernel(ushort *vtd0, ushort *vtd1, ushort *nbrs, ushort *nbr_rng) {
+
+    __global__ void gerrymander_kernel(ushort *vtd0, ushort *vtd1, ushort *nbrs, ushort *nbr_rng, ushort *pop_vtd, uint *rand_seeds) {
         const ushort threads_per_block = blockDim.z * blockDim.y * blockDim.x;
         const ushort blockId = blockIdx.z*(gridDim.y*gridDim.x) + blockIdx.y*(gridDim.x) + blockIdx.x;
         const ushort threadIdLoc = threadIdx.z*(blockDim.y*blockDim.x) + threadIdx.y*(blockDim.x) + threadIdx.x;
         const uint threadIdGlob = blockId*threads_per_block + threadIdLoc;
         
-        ushort w;
+        curandState rand_state;
+        curand_init(rand_seeds[threadIdGlob], threadIdGlob, 0, &rand_state);
         
+        ushort u, v, w, c;
         
         __shared__ ushort nbrs_shr[NUM_NBRS];
         to_shared(nbrs, nbrs_shr, NUM_NBRS, threadIdLoc);
@@ -148,64 +166,213 @@ kernel_code = """
             print_shared(nbr_rng_shr, NUM_VTDS+1);
             printf("\\n");
         }
+
+        __shared__ ushort pop_vtd_shr[NUM_VTDS];
+        to_shared(pop_vtd, pop_vtd_shr, NUM_VTDS, threadIdLoc);
+        if(threadIdLoc == 0) {
+            printf("Printing A_shr = pop_vtd_shr\\n");
+            print_shared(pop_vtd_shr, NUM_VTDS);
+            printf("\\n");
+        }
         
-        ushort color[NUM_VTDS];
-        color[0] = 0;
-        color[1] = 0;
-        color[2] = 0;
         
-        ushort component[NUM_VTDS];
-        ushort num_comps = NUM_VTDS;
+        ushort clr[NUM_VTDS];
+        clr[0] = 0;
+        clr[1] = 1;
+        clr[2] = 1;
+        clr[3] = 0;
+        clr[4] = 0;
+        
+        ushort cmp[NUM_VTDS];
+        ushort num_cmps = NUM_VTDS;
         for(w = 0; w < NUM_VTDS; w++) {
-            component[w] = w;
+            cmp[w] = w;
         }
         
         for(w = 0; w < NUM_VTDS; w++) {
-            printf("There are currently %d connected components\\n",num_comps);
-            get_components_local(nbrs_shr, nbr_rng_shr, color, component, &num_comps, w);
-            printf("There are currently %d connected components\\n",num_comps);
-            printf("\\n\\n");
+            printf("DIV1");
+            get_cmps_local(nbrs_shr, nbr_rng_shr, clr, cmp, &num_cmps, w);
+            printf("There are currently %u connected cmps\\nDIV1",num_cmps);
+        }        
+        
+        ushort pop_clr[NUM_CLRS];
+        for(c = 0; c < NUM_CLRS; c++) {
+            pop_clr[c] = 0;
         }
+        
+        float pop_tot = 0;
+        for(w = 0; w < NUM_VTDS; w++) {
+            pop_clr[clr[w]] += pop_vtd_shr[w];
+            pop_tot += pop_vtd_shr[w];
+        }
+        
+        float unif_dens = 1 / (float)NUM_CLRS;
+        float TV_vec[NUM_CLRS];
+        float TV = 0;
+        for(c = 0; c < NUM_CLRS; c++) {
+            get_TV_local(pop_clr, pop_tot, unif_dens, TV_vec, c);
+            TV += TV_vec[c];
+        }
+        
+        printf("unif_dens=%f\\n",unif_dens);
+        for(c = 0; c < NUM_CLRS; c++) {
+            printf("clr %u: pop=%u, dens=%f, TV_local=%f\\n", c, pop_clr[c], pop_clr[c]/pop_tot, TV_vec[c]);            
+        }
+        printf("TV=%f\\n",TV);
+        printf("DIV1");
+
+        ushort step = 0;
+        ushort draw = 0;
+        ushort const max_draws = 10;
+        ushort edge = 0;
+        float r;
+        print_all(clr, cmp);
+        for(step = 0; step < MAX_STEPS; step++) {
+            for(draw = 0; draw < max_draws; draw++) {
+                edge = (ushort)(curand_uniform(&rand_state) * NUM_EDGES);
+                v = vtd0[edge];
+                w = vtd1[edge];
+                printf("I choose edge %u between vtd%u:clr%u:cmp%u and vtd%u:clr%u:cmp%u,\\n", edge, v, clr[v], cmp[v], w, clr[w], cmp[w]);
+                if(clr[v] != clr[w]){
+                    printf("clrs differ!! Moving on\\n");
+                    break;
+                }
+            }
+            if(draw >= max_draws) {
+                printf("ERROR - could not find edge with different clrs\\n");
+                break;;
+            }
+            r = curand_uniform(&rand_state); 
+            if(r <= 0.5) {
+                printf("I drew r = %f <= 0.5, so vtd%u:clr%u -> clr%u\\n\\n", r, w, clr[w], clr[v]);                
+            } 
+            else {
+                //Swap v & w
+                u = v;
+                v = w;
+                w = u;
+                printf("I drew r = %f > 0.5, so vtd%u:clr%u -> clr%u\\n\\n", r, w, clr[w], clr[v]);                
+            }
+            for(c = 0; c < NUM_CLRS; c++) {
+                printf("clr%u:pop%u:TV%f  ",c, pop_clr[c], TV_vec[c]);
+            }
+            printf("  TV=%f\\n\\n",TV);
+            printf("Pop%u=%u  clr%u -> clr%u\\n\\n", w, pop_vtd[w], clr[w], clr[v]);
+            pop_clr[clr[v]] += pop_vtd[w];
+            pop_clr[clr[w]] -= pop_vtd[w];
+
+            TV -= TV_vec[v];
+            TV -= TV_vec[w];
+            get_TV_local(pop_clr, pop_tot, unif_dens, TV_vec, clr[v]);
+            get_TV_local(pop_clr, pop_tot, unif_dens, TV_vec, clr[w]);
+            TV += TV_vec[v];
+            TV += TV_vec[w];            
+            clr[w] = clr[v];
+            for(c = 0; c < NUM_CLRS; c++) {
+                printf("clr%u:pop%u:TV%f  ",c, pop_clr[c], TV_vec[c]);
+            }
+            printf("  TV=%f\\n",TV);
+            printf("DIV1");
+            get_cmps_local(nbrs_shr, nbr_rng_shr, clr, cmp, &num_cmps, w);
+            
+            
+            printf("DIV1After step %u, num components = %u and TV = %f\\n", step, num_cmps, TV);
+            print_all(clr, cmp);
+            printf("DIV2DIV2");
+        }
+
     }
+}
 """
 
 
-blockDims = (1,1,1)
 gridDims = (1,1,1)
+blockDims = (1,1,1)
+max_steps = 2
+num_sims = np.product([gridDims,blockDims])
+rand_seeds = np.arange(num_sims)
 
-num_colors = 2
+num_clrs = 3
+pop_vtd = 3 * np.arange(5)
 
-num_edges = 20
-vtd0 = np.zeros(num_edges)
-vtd1 = np.zeros(num_edges)
-num_edges = len(vtd0)
-if len(vtd1) != num_edges:
-    raise Exception("Mismatch: length of vtd0 = %d, length of vtd1 = %d"%(len(vtd0), len(vtd1)))
+num_vtds = len(pop_vtd)
 
-nbrs = [[1]
-       ,[0,2]
-       ,[1]
+#If given edge list
+edges = [(0,4)
+        ,(1,4)
+        ,(2,4)
+        ,(4,3)
+        ,(3,4)        
+        ]
+
+#If given neighbor list
+"""
+nbrs = [[1,4]
+       ,[0,4]
+       ,[4]
+       ,[4]
+       ,[0,1,2,3]
        ]
-num_vtds = len(nbrs)
-degree = np.array([len(n) for n in nbrs])
-nbr_rng = np.insert(degree.cumsum(),0,0)
+edges = [(v,w) for (v,N) in enumerate nbrs for w in N if v<w]
+"""
 
+for (idx,e) in enumerate(edges):
+    if(len(e) != 2):
+        raise Exception('edge %d: %s is not length 2'%(idx, e))
+    if((e[0]%1 != 0) | (e[1]%1 != 0)):
+        raise Exception('edge %d: %s contains a non-integer'%(idx, e))
+
+
+edges = [tuple(sorted(e)) for e in edges if e[0] != e[1]] #make edge (smaller,larger) & remove self-loops
+edges = set(edges) #removes duplicates
+edges = sorted(list(edges)) #sorts
+edges = np.array(edges).astype('uint16')
+
+if num_vtds != edges.max()+1:
+    raise Exception('length of population vector != max vtd in edge list.  Is there an isolated vtd?')
+
+vtd0 = edges[:,0]
+vtd1 = edges[:,1]
+num_edges = len(vtd0)
+
+nbrs = [[] for v in range(num_vtds)]
+for e in edges:
+    nbrs[e[0]].append(e[1])
+    nbrs[e[1]].append(e[0])
+
+degree = np.array([len(n) for n in nbrs])
+nbr_rng = np.insert(np.cumsum(degree),0,0)
+
+
+print("edges\n%s"%edges)
+print("num_edges = %s"%num_edges)
+print("vtd0 = %s"%vtd0)
+print("vtd1 = %s"%vtd1)
+print("num_vtds = %s"%num_vtds)
+print("neighbors = %s"%nbrs)
+print("degree = %s"%degree)
+print("neighbor index range = %s"%nbr_rng)
+
+rand_seeds_gpu = togpu(rand_seeds,'uint32')
 vtd0_gpu = togpu(vtd0,'uint16')
 vtd1_gpu = togpu(vtd1,'uint16')
 nbrs_gpu = togpu(np.concatenate(nbrs),'uint16')
 nbr_rng_gpu = togpu(nbr_rng,'uint16')
+pop_vtd_gpu = togpu(pop_vtd,'uint16')
+
 
 macros = {'DIV1':"*" * 80 + "\\n"
          ,'DIV2':"#" * 80 + "\\n"
          ,'NUM_VTDS':num_vtds
          ,'NUM_NBRS':len(nbrs_gpu)
-         ,'NUM_COLORS':num_colors
+         ,'NUM_CLRS':num_clrs
          ,'NUM_EDGES':num_edges
+         ,'MAX_STEPS':max_steps
          }
 for key, val in macros.items():
     kernel_code = kernel_code.replace(str(key),str(val))
 
-mod = SourceModule(kernel_code);
+mod = SourceModule(kernel_code, no_extern_c=True);
 gerrymander = mod.get_function("gerrymander_kernel")
 
-gerrymander(vtd0_gpu, vtd1_gpu, nbrs_gpu, nbr_rng_gpu, block=blockDims, grid=gridDims, shared=0)
+gerrymander(vtd0_gpu, vtd1_gpu, nbrs_gpu, nbr_rng_gpu, pop_vtd_gpu, rand_seeds_gpu, block=blockDims, grid=gridDims, shared=0)
