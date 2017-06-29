25228dd986955fdd18748912cc5f29ea5b301599
marybarker <marybarker103@gmail.com>
2017-06-29 11:44:58 -0500

    reordered NC setup to be more coherent

--- NorthCarolina/setup.py
+++ NorthCarolina/setup.py
@@ -2,19 +2,14 @@ stateSHORT = 'NC'
 ndistricts = 13
 
 blockstats = pd.read_csv('./vtdstats.csv', dtype={"VTD":str}).rename(columns={"ALAND10":"ALAND", "AWATER10":"AWATER"})
-blockstats = blockstats.set_index(blockstats.ID)
-
+blockstats = blockstats.set_index(blockstats.ID)n Libs", "VR: Mult Race Ls", \
+                   "VR:All Unaf.", "VR: White Unafil.", "VR: Black Unafil.", "VR: Asian Unafil.", "VR: Other Unafil."
+                  ]]
 #GEE = pd.read_csv("RegGenderAgeEthnicity.csv")
 PRE = pd.read_csv("TotalPopRaceAndEthnicity.csv")
 #VAP = pd.read_csv("VotingAgePopulation.csv")
 RPR = pd.read_csv("RegPartyRace.csv")
 
-RPR = RPR.loc[:, ["GEOID10", "VR Total", \
-                   "VR: All Dems", "VR: White Dems", "VR: Black Dems", "VR: Asian Dems", "VR: Mult Race Ds", \
-                   "VR: All Reps", "VR: White Reps", "VR: Black Reps", "VR: Asian Reps", "VR: Mult Race Rs", \
-                   "VR: All Libs", "VR: White Libs", "VR: Black Libs", "VR: Asian Libs", "VR: Mult Race Ls", \
-                   "VR:All Unaf.", "VR: White Unafil.", "VR: Black Unafil.", "VR: Asian Unafil.", "VR: Other Unafil."
-                  ]]
 #totalVR = RPR.loc[:, ['VR: All Dems', 'VR: All Reps', 'VR: All Libs', 'VR:All Unaf.']].sum(axis=1)
 totalVR = RPR['VR Total']
 RPR['percentDem'] = RPR['VR: All Dems'] / totalVR
@@ -22,6 +17,11 @@ RPR['percentRep'] = RPR['VR: All Reps'] / totalVR
 RPR['percentOther'] = 1.0 - RPR.loc[:, ['percentDem', 'percentRep']].sum(axis=1)
 
 RPR['DemPercentWhite'] = RPR['VR: White Dems'] / RPR['VR: All Dems']
+
+RPR = RPR.loc[:, ["GEOID10", "VR Total", \
+                   "VR: All Dems", "VR: White Dems", "VR: Black Dems", "VR: Asian Dems", "VR: Mult Race Ds", \
+                   "VR: All Reps", "VR: White Reps", "VR: Black Reps", "VR: Asian Reps", "VR: Mult Race Rs", \
+                   "VR: All Libs", "VR: White Libs", "VR: Black Libs", "VR: Asia
 RPR['DemPercentBlack'] = RPR['VR: Black Dems'] / RPR['VR: All Dems']
 RPR['DemPercentAsian'] = RPR['VR: Asian Dems'] / RPR['VR: All Dems']
 RPR['DemPercentOther'] = 1.0 - RPR.loc[:, ['DemPercentWhite', 'DemPercentBlack', 'DemPercentAsian']].sum(axis=1)
@@ -36,6 +36,7 @@ RPR = RPR.loc[:, ['GEOID10', 'percentDem', 'percentRep', 'percentOther', \
 
 PRE = PRE.loc[:, ['Total', 'GEOID10', '% Total Black', '%  Hisp ']].rename(columns={'Total':'population', '% Total Black':'aframcon', '%  Hisp ':'hispcon'})
 PRE["mincon"] = PRE.loc[:, ['aframcon', 'hispcon']].sum(axis=1)
+PRE.mincon[PRE.mincon > 1.0] = 1.0
 
 blockstats = pd.merge(blockstats, pd.merge(PRE, RPR, on='GEOID10'), on='GEOID10')
 totalpopulation = sum(blockstats.population)
