4abb7f62d9194e10f0580c375f6fb2c97b939c47
marybarker <marybarker103@gmail.com>
2017-08-17 11:05:39 -0500

    Starting to merge demographic info with the google maps data structure

--- webpage/pageGlobals.js
+++ webpage/pageGlobals.js
@@ -50,13 +50,52 @@ function calculateAll(dist, funcName){
         totArea += feature.getProperty("ALAND10");
       }
     });
-    outputString += ("Total Area: " + totArea.toFixed(3).toString());
+    outputString += ("Total Area: " + totArea.toFixed(3).toString()+'<br>');
+  }
+  if(funcName == "aframcon"){
+    var totConc = 0.0;
+    var toDivide = 0;
+    var idName, addValue,divValue;
+
+    map.data.forEach(function(feature){
+      var val = feature.getProperty("District");
+      if (val == dist){
+        idName = feature.getProperty("GEOID10");
+
+        divValue = parseFloat(blockstats[idName].population);
+        addValue = parseFloat(blockstats[idName].aframcon);
+
+        toDivide += 1.0 * divValue;
+        totConc += (divValue * addValue);
+      }
+    });
+    totConc /= toDivide;
+    outputString += ("African American Concentration: "+totConc.toFixed(3).toString()+'<br>');
+  }
+  if(funcName == "hispcon"){
+    var totConc = 0.0;
+    var toDivide = 0;
+    var idName, addValue,divValue;
+
+    map.data.forEach(function(feature){
+      var val = feature.getProperty("District");
+      if (val == dist){
+        idName = feature.getProperty("GEOID10");
+
+        divValue = parseFloat(blockstats[idName].population);
+        addValue = parseFloat(blockstats[idName].hispcon);
+
+        toDivide += 1.0 * divValue;
+        totConc += (divValue * addValue);
+      }
+    });
+    totConc /= toDivide;
+    outputString += ("Hispanic Concentration: "+totConc.toFixed(3).toString()+'<br>');
   }
   return outputString;
 }
 
 function addToList(thingy){
-  var values = document.getElementById("checkStats");
   if(thingy.checked){
     list_of_functions_to_compute.push(thingy.value);
   } else {
@@ -64,16 +103,57 @@ function addToList(thingy){
   }
 }
 
-var colList = ["#000000"];
-var jsonfilename="./NCPrecincts.json";
-var connectionsfilename="../NorthCarolina/PRECINCTConnections.csv"
-var centroid=[35.0, -79.9];
-var map;
-var currentDistrict="Not Assigned";
-var districts=["Not Assigned",0];
-var allColors={"Not Assigned":"#000000", 0:add_new_color()}
+function zip(a, b){
+  var arr = {};
+  for(var key in a){
+    arr[a[key]] = b[key];
+  } 
+  return arr;
+}
 
+function loadCSVtoArrays(data){
+  var allRows = data.split("\n");
+  var idIdx;
+  var toReturn;
+  var otherList = [];
+  
+  for(var singleRow=0; singleRow < allRows.length; singleRow++){
+    var rowCells = allRows[singleRow].split(',');
+    
+    if (singleRow === 0){
+      idIdx = rowCells.indexOf(indexingCol);
+      otherList = [for (x of listOfCols) rowCells.indexOf(x)];
+    }else{
+      blockstats[rowCells[idIdx]] = zip(listOfCols, [for (x of otherList) rowCells[x]]);
+    }
+  }
+}
+
+/* Don't need generalizing */
+var map;
 var yet_to_assign;
 var list_of_functions_to_compute = [];
+
+/* depends on which state you want */
+var jsonfilename="./NC/NCPrecincts.json";
+var connectionsfilename="./NC/PRECINCTconnections.csv"
+var statsfilename="./NC/vtdstats.csv"
+var centroid=[35.0, -79.9];
+var indexingCol = "GEOID10";
+var listOfCols = ["ID", "ALAND", "aframcon", "hispcon", "population"];
+var blockstats = {};
+
+/* depend on the current coding setup */
+var colList = ["#000000"];
+var currentDistrict="Not Assigned";
+var districts=["Not Assigned",0];
+var allColors={"Not Assigned":"#000000", 0:add_new_color()};
+
+$.ajax({
+  url:statsfilename,
+  dataType:'text',
+}).done(loadCSVtoArrays)
+
+//console.log(blockstats);
 
 
